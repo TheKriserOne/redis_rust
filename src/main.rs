9247d905df1e0a6dd72ac9df9f8f03c0d9f6276b
TheKriserOne <dimazhgirov45632@gmail.com>
2025-06-15 16:52:40 -0700

    PONG

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,6 @@
 #![allow(unused_imports)]
+
+use std::io::Write;
 use std::net::TcpListener;
 
 fn main() {
@@ -9,9 +11,10 @@ fn main() {
     //
     let listener = TcpListener::bind("127.0.0.1:6379").unwrap();
 
-    for stream in listener.incoming() {
-        match stream {
-            Ok(_stream) => {
+    for conn in listener.incoming() {
+        match conn {
+            Ok(mut stream) => {
+                stream.write_all(b"+PONG\r\n").unwrap();
                 println!("accepted new connection");
             }
             Err(e) => {
