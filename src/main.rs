8e5d2a1b1de62935dbdce5447e70d5f256f615e6
TheKriserOne <dimazhgirov45632@gmail.com>
2025-06-15 21:41:26 -0700

    PONG

--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,75 @@
+use std::sync::{Arc, Weak};
 use tokio::io::AsyncWriteExt;
-use tokio::net::TcpListener;
+use tokio::join;
+use tokio::net::{TcpListener, TcpStream};
+use tokio::sync::Mutex;
+use tokio::task::JoinHandle;
+use tokio_stream::wrappers::TcpListenerStream;
+use tokio_stream::{wrappers, StreamExt};
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
     println!("Logs from your program will appear here!");
 
-
     let listener = TcpListener::bind("127.0.0.1:6379").await?;
-    println!("Listening on: 127.0.0.1:6379");
+    let stream_of_conn = TcpListenerStream::new(listener);
+    let handles = Arc::new(Mutex::new(Vec::new()));
+    join!(
+        listen(stream_of_conn, handles.clone()),
+        stream_connection(handles.clone())
+    );
+    Ok(())
 
+    // Spawn a new task for each connection
+    // tokio::spawn(async move {
+    //     // Send PONG response
+    //     if let Err(e) = socket.write_all(b"+PONG\r\n").await {
+    //         println!("Error writing to socket: {}", e);
+    //     }
+    // });
+}
 
+async fn listen(
+    mut listener: TcpListenerStream,
+    handles: Arc<Mutex<Vec<TcpStream>>>,
+) -> Result<(), Box<dyn std::error::Error>> {
+    while let Some(Ok(mut stream)) = listener.next().await {
+        stream.write_all(b"Connected").await?;
+        let mut handles = handles.lock().await;
+        handles.push(stream);
+    }
+    Ok(())
+}
+async fn stream_connection(handles: Arc<Mutex<Vec<TcpStream>>>) {
     loop {
-        let (mut socket, addr) = listener.accept().await?;
-        println!("Accepted connection from: {}", addr);
-
-        // Spawn a new task for each connection
-        tokio::spawn(async move {
-            // Send PONG response
-            if let Err(e) = socket.write_all(b"+PONG\r\n").await {
-                println!("Error writing to socket: {}", e);
+        let mut streams_to_process = {
+            let mut streams = handles.lock().await;
+            if streams.is_empty() {
+                continue;
             }
-        });
+            std::mem::take(&mut *streams) // Take all streams, leaving Vec empty
+        };
+
+        let _: Vec<_> = streams_to_process
+            .into_iter()
+            .map(|mut stream| {
+                tokio::spawn(async move {
+                    loop {
+                        if let Err(_) = stream.write_all(b"+PONG\r\n").await {
+                            println!("Error writing to socket, closing connection.");
+                            return;
+                        }
+                        tokio::time::sleep(tokio::time::Duration::from_secs(5)).await;
+                    }
+                });
+            })
+            .collect();
     }
 }
+// async fn send_pong(socket: &mut TcpStream) -> JoinHandle<()> {
+//     tokio::spawn(async move {
+//         if let Err(e) = socket.write_all(b"+PONG\r\n").await {
+//             println!("Error writing to socket: {}", e);
+//         }
+//     })
+// }
