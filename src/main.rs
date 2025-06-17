664e1e49026e7d00e9a40fa18a1927bae6126805
TheKriserOne <dimazhgirov45632@gmail.com>
2025-06-16 22:10:22 -0700

    PING

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+mod parser;
+
 use std::sync::{Arc, Weak};
 use tokio::io::{AsyncReadExt, AsyncWriteExt};
 use tokio::join;
@@ -6,6 +8,7 @@ use tokio::sync::Mutex;
 use tokio::task::JoinHandle;
 use tokio_stream::wrappers::TcpListenerStream;
 use tokio_stream::{wrappers, StreamExt};
+use crate::parser::RESPtypes;
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
@@ -56,13 +59,38 @@ async fn stream_connection(handles: Arc<Mutex<Vec<TcpStream>>>) {
                     loop {
                         let mut buffer= vec![0u8; 1024];; // Buffer to read data
                         while let Ok(n) = stream.read(&mut buffer).await {
-                            stream
-                                .write_all(b"+PONG\r\n")
-                                .await
-                                .expect("Failed to write PONG response");
-                            let received = String::from_utf8(Vec::from(&buffer[..n])).unwrap();
-                            println!("Received: {}", received);
 
+                            let received = String::from_utf8(Vec::from(&buffer[..n])).unwrap();
+                            let parsed = RESPtypes::from_string(&received);
+                            match parsed {
+                                RESPtypes::Arrays(val) => {
+                                    // if let Some(RESPtypes::BulkStrings(s)) = &val[0] {
+                                    //
+                                    // }
+
+                                    let mut val = val.iter();
+                                    if let Some(RESPtypes::BulkStrings(s)) = val.next() {
+                                        if s == "ECHO" {
+                                            let echoed_val = val.fold("".to_string(), |initial, current| {
+                                                initial + &*current.to_resp_string()
+                                            });
+                                            stream
+                                                .write_all(echoed_val.as_ref())
+                                                .await
+                                                .expect("Failed to write PONG response");
+                                        }
+                                        else if s == "PING" {
+                                            stream
+                                                .write_all(b"+PONG\r\n")
+                                                .await
+                                                .expect("Failed to write PONG response");
+                                        }
+                                    }
+                                }
+                                _ => {
+                                    stream.write_all(b"-ERR Protocol error: invalid RESP format\r\n").await.unwrap();
+                                }
+                            }
                         }
 
                     }
---
+++ src/parser.rs
@@ -0,0 +1,51 @@
+use std::vec::IntoIter;
+
+#[derive(Debug)]
+pub enum RESPtypes {
+    BulkStrings(String),
+    Arrays(Vec<RESPtypes>),
+}
+
+impl RESPtypes {
+    pub fn from_string(input: &str) -> Self {
+        RESPtypes::decode(&mut RESPtypes::tokenize(&input))
+    }
+    pub fn tokenize(input: &str) -> IntoIter<&str> {
+        input.split("\r\n").collect::<Vec<&str>>().into_iter()
+    }
+    pub fn decode(tokens: &mut IntoIter<&str>) -> Self {
+        if let Some(val) = tokens.next() {
+            if val.chars().nth(0) == Option::from('*') {
+                let num: i32 = *&val[1..].parse().unwrap();
+                let mut vector = Vec::new();
+                for i in 0..num {
+                    vector.push(Self::decode(tokens));
+                }
+                return Self::Arrays(vector);
+            } else if val.chars().nth(0) == Option::from('$') {
+                return Self::BulkStrings(String::from(tokens.next().unwrap()));
+            }
+        }
+        Self::BulkStrings(String::from("non"))
+    }
+    pub fn to_resp_string(&self) -> String {
+        match &self {
+            RESPtypes::BulkStrings(val) => format!("${}\r\n{}\r\n", val.len(), val),
+            RESPtypes::Arrays(val) => {
+                val.iter().fold(format!("*{}\r\n", val.len()), |initial, current| {
+                    initial + &*current.to_resp_string()
+                })
+            }
+        }
+    }
+    pub fn to_string(&self) -> String {
+        match &self {
+            RESPtypes::BulkStrings(val) => val.to_owned(),
+            RESPtypes::Arrays(val) => {
+                val.iter().fold("".to_string(), |initial, current| {
+                    initial + &*current.to_string() + " "
+                })
+            }
+        }
+    }
+}
